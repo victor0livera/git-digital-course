6ccd6fec21db91e093263079e17327c82e8fa7a9
victor <jv81332@gmail.com>
2023-11-09 14:08:53 -0300

    add new title

---
+++ gravandoMudancasNoRepositoriio.js
@@ -0,0 +1,17 @@
+/*
+ESTADOS DO GIT/VERSIONAMENTO
+
+                Untracked       unmodified      Modified        Staged
+add the file        =>              =>             =>
+
+              edit the file         =>
+                              stage the file       =>
+remove the file    <==
+                                    <==            <==         <==         commit
+                                        
+salvando modificacoes no git
+
+COMANDOS GIT:
+git add .\{nome do  arquivo}
+git diff
+*/
